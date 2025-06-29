a9c7ae62189820ab9fac0c887856a0b2738e4327
Huy Le <legiahuy050104@gmail.com>
2025-06-29 09:35:21 -0400

    doc string added to main.tsx

--- src/frontend/main.tsx
+++ src/frontend/main.tsx
@@ -1,7 +1,12 @@
+/**
+ * This is where App.tsx is used.
+ * @author Huy Le (huyisme-005)
+ */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+//createRoot lets you create a root to display React components inside a browser DOM node.
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
