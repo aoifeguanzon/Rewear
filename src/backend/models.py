a675f4157293694bdb223a39d73059d785079b04
Valerie Pena <valeriepena@Valeries-MacBook-Pro.local>
2025-07-02 14:05:02 -0400

    Implement user signup & login with hashed passwords and DynamoDB (Valerie)

---
+++ src/backend/models.py
@@ -0,0 +1,12 @@
+from pynamodb.models import Model
+from pynamodb.attributes import UnicodeAttribute
+
+class UserModel(Model):
+    class Meta:
+        table_name = "rewear-users-v2"
+        region = "us-east-1"
+
+    username = UnicodeAttribute(hash_key=True)
+    email = UnicodeAttribute()    
+    password = UnicodeAttribute()
+
