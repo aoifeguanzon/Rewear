c78836255a37dae69883386b754802429f330d10
huyisme-005 <legiahuy050104@gmail.com>
2025-08-12 15:52:04 -0400

    Vercel Analytics added

--- src/frontend/main.tsx
+++ src/frontend/main.tsx
@@ -6,9 +6,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
 import App from './App';
+import { Analytics } from '@vercel/analytics/react'; // Added for Vercel Analytics
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Analytics /> {/* Add this line to enable Vercel Analytics */}
   </React.StrictMode>
 );
